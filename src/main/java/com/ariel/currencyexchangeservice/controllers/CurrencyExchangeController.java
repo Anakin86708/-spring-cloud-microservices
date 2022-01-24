80476ff79d723c0ea8babb418ff24e7b92bf8713
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-24 08:51:41 -0300

    Obtendo valor da porta dinamicamente para a resposta.

--- src/main/java/com/ariel/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ src/main/java/com/ariel/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.ariel.currencyexchangeservice.controllers;
 
 import com.ariel.currencyexchangeservice.models.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,15 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment env;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new CurrencyExchange(100L, from, to, BigDecimal.valueOf(5.46));
+        CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(5.46));
+        String port = env.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 }
