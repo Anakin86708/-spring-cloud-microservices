1459018af7ad02b6df0b0c3322343465777eed1c
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-24 10:43:20 -0300

    Utilizando Feing para obter os dados do outro microserviço.

--- src/main/java/com/ariel/currencyconversionservice/controllers/CurrencyConversionController.java
+++ src/main/java/com/ariel/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -1,31 +1,25 @@
 package com.ariel.currencyconversionservice.controllers;
 
 import com.ariel.currencyconversionservice.models.CurrencyConversion;
-import org.springframework.http.ResponseEntity;
+import com.ariel.currencyconversionservice.proxies.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy exchangeProxy;
+
     @GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-        CurrencyConversion currencyConversion = getCurrencyConversion(from, to);
+    public CurrencyConversion retrieveCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        CurrencyConversion currencyConversion = exchangeProxy.retrieveExchangeValue(from, to);
         BigDecimal conversionMultiple = currencyConversion.getConversionMultiple();
         return new CurrencyConversion(currencyConversion.getId(), from, to, conversionMultiple, quantity, calculate(quantity, conversionMultiple), currencyConversion.getEnvironment());
-    }
-
-    private CurrencyConversion getCurrencyConversion(String from, String to) {
-        String url = String.format("http://localhost:8000/currency-exchange/from/%s/to/%s/", from, to);
-        HashMap<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-        ResponseEntity<CurrencyConversion> response = new RestTemplate().getForEntity(url, CurrencyConversion.class, uriVariables);
-        return response.getBody();
     }
 
     private BigDecimal calculate(BigDecimal quantity, BigDecimal conversionMultiple) {
