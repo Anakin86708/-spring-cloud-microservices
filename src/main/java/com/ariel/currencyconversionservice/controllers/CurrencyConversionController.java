510753bd8e76bc6726b19bb0390b58bd553237b1
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-26 12:56:09 -0300

    Modificações para o deploy pelo Google Cloud.

--- src/main/java/com/ariel/currencyconversionservice/controllers/CurrencyConversionController.java
+++ src/main/java/com/ariel/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -2,6 +2,8 @@ package com.ariel.currencyconversionservice.controllers;
 
 import com.ariel.currencyconversionservice.models.CurrencyConversion;
 import com.ariel.currencyconversionservice.proxies.CurrencyExchangeProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +14,14 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyConversionController {
 
+    private final Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
+
     @Autowired
     private CurrencyExchangeProxy exchangeProxy;
 
     @GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion retrieveCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        logger.info("Converting {} from {} to {}", quantity, from, to);
         CurrencyConversion currencyConversion = exchangeProxy.retrieveExchangeValue(from, to);
         BigDecimal conversionMultiple = currencyConversion.getConversionMultiple();
         return new CurrencyConversion(currencyConversion.getId(), from, to, conversionMultiple, quantity, calculate(quantity, conversionMultiple), currencyConversion.getEnvironment());
